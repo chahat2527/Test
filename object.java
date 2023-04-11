e7bf8218487b1e2a34853079d442e4e19eb4dc77
CHAHAT2725 <129862201+CHAHAT2725@users.noreply.github.com>
2023-04-11 23:21:18 +1000

    4th class file

---
+++ object.java
@@ -0,0 +1,13 @@
+class CreateObject {
+    void show() {
+        System.out.println("welcome to javaTpoint");
+    }
+
+    public static void main(String[] args)
+    {
+        //creating an object using new keyword
+        CreateObject obj = new Create object();
+        //invoking method using the object
+        obj.show();
+    }
+}
