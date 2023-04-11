a5fc12ae906c2797706df4b0f271b06df09cad38
CHAHAT2725 <129862201+CHAHAT2725@users.noreply.github.com>
2023-04-11 17:47:53 +1000

    addition of 2 nub's

---
+++ addition.java
@@ -0,0 +1,7 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int a = 5, b = 20, sum;
+        sum = a + b;
+        System.out.println("Sum of these numbers:" + sum);
+    }
+}
