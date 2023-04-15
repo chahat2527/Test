777d92b327f5f55f11515f9d5652da06a4488dd2
chahat2527 <chahat27bhandari@gmail.com>
2023-04-16 01:03:05 +1000

    ist commit

--- object.java
+++ object.java
@@ -1,13 +1,13 @@
-class CreateObject {
-    void show() {
-        System.out.println("welcome to javaTpoint");
+public class createObject
+{
+    //constructor of the class
+    createObject
+    {
+        System.out.println("welcome to javapoint");
     }
-
     public static void main(String[] args)
     {
         //creating an object using new keyword
-        CreateObject obj = new Create object();
-        //invoking method using the object
-        obj.show();
+        Createobject obj= new CreateObject();
     }
-}
+    }
